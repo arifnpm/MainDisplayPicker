54e6e99a0ed4928791e9c9a5795bbb023ab90cfb
arif.masyur <arif.masyur@azentio.com>
2023-12-16 15:12:16 +0700

    Change title

--- MainDisplayPicker/Form1.Designer.cs
+++ MainDisplayPicker/Form1.Designer.cs
@@ -46,13 +46,13 @@ namespace MainDisplayPicker
             // 
             this.label1.BackColor = System.Drawing.Color.Black;
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(440, 102);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Display Manager";
+            this.label1.Text = "Drag this window to one monitor and select one of the action below";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnSetAsMain
